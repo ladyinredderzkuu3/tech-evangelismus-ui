9abfdb382e911a6fa569beee78a0e6ba50ced749
ladyinredderzkuu3 <mwineesref@hotmail.com>
2025-08-14 22:34:03 -0500

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Willkommen bei Tech Evangelismus UI</h1>
+      <p>Lerne Technologie auf interaktive Weise!</p>
+    </div>
+  );
+}
+
+export default App;
