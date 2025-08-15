6f0428f46b53b31e55d43c3185d0c5a6e236daf6
ladyinredderzkuu3 <mwineesref@hotmail.com>
2025-08-14 22:34:05 -0500

    feat: initial commit for src/components/Quiz.js

---
+++ src/components/Quiz.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Quiz = () => {
+  return (
+    <div>
+      <h2>Quiz zur Überprüfung des Wissens</h2>
+      <p>Hier wird das Quiz angezeigt.</p>
+    </div>
+  );
+}
+
+export default Quiz;
