723332268628a1ae0838a54850aea4193c6024c2
ladyinredderzkuu3 <mwineesref@hotmail.com>
2025-08-14 22:34:06 -0500

    feat: initial commit for src/components/Forum.js

---
+++ src/components/Forum.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Forum = () => {
+  return (
+    <div>
+      <h2>Community-Forum</h2>
+      <p>Hier kann die Community Ideen und Fragen austauschen.</p>
+    </div>
+  );
+}
+
+export default Forum;
